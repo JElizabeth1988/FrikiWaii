e4b8a818122d99698f4952b1d0add5c02855acad
JElizabeth1988 <johana.elizabeth1988@gmail.com>
2019-10-26 01:05:58 -0300

    urls

--- dir_frikiwaiiStore/FrikiWaii/core/urls.py
+++ dir_frikiwaiiStore/FrikiWaii/core/urls.py
@@ -1,8 +1,34 @@
 from django.urls import path
-from .views import home
+from .views import home, alien, audifonos, bunny, car_avengers, car_naruto,
+carro, crybaby, desc_accesorios, desc_bags, desc_gamerstyle, desc_outfits,
+desc_papeleria, desc_phonecase, inosuke, joker, llavero, memo, momo, muerto,
+penny, pokemochila, sacapuntas, sudadera
 
 urlpatterns = [
     path('', home, name="home"),
-    # path('registro/', registro, name="registro"),
-    # path('listar/', listar_automoviles, name="listar"),
+    path('Alien/', alien, name="alien"),
+    path('Audifonos/', audifonos, name="audifonos"),
+    path('Mascara-bunny/', bunny, name="bunny"),
+    path('Carcasa-avengers/', car_avengers, name="car-avengers"),
+    path('Carcasa-naruto/', car_naruto, name="car_naruto"),
+    path('Carro/', carro, name="carro"),
+    path('Mascara-cry-baby/', crybaby, name="crybaby"),
+    path('Accesorios/', desc_accesorios, name="desc_accesorios"),
+    path('Bags/', desc_bags, name="desc_bags"),
+    path('GamerStyle/', desc_gamerstyle, name="desc_gamerstyle"),
+    path('Outfits/', desc_outfits, name="desc_outfits"),
+    path('Papeleria/', desc_papeleria, name="desc_papeleria"),
+    path('PhoneCase/', desc_phonecase, name="desc_phonecase"),
+    path('Inosuke/', inosuke, name="inosuke"),
+    path('Mascara-Joker/', joker, name="joker"),
+    path('llavero/', llavero, name="llavero"),
+    path('memo-Gato/', memo, name="memo"),
+    path('Mascara-momo/', momo, name="momo"),
+    path('mascara-muerto/', muerto, name="muerto"),
+    path('mascara-pennywise/', penny, name="penny"),
+    path('pokemochila/', pokemochila, name="pokemochila"),
+    path('sacapuntas/', sacapuntas, name="sacapuntas"),
+    path('sudadera-avengers/', sudadera, name="sudadera"),
+    
+  
 ]
--- dir_frikiwaiiStore/FrikiWaii/core/views.py
+++ dir_frikiwaiiStore/FrikiWaii/core/views.py
@@ -3,3 +3,74 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'core/home.html')
+
+def alien(request):
+    return render(request, 'core/alien.html')
+
+def audifonos(request):
+    return render(request, 'core/audifonos.html')
+
+def bunny(request):
+    return render(request, 'core/bunny.html')
+
+def car_avengers(request):
+    return render(request, 'core/car-avengers.html')
+
+def car_naruto(request):
+    return render(request, 'core/car-naruto.html')
+
+def carro(request):
+    return render(request, 'core/carro.html')
+
+def crybaby(request):
+    return render(request, 'core/crybaby.html')
+
+def desc_accesorios(request):
+    return render(request, 'core/desc-accesorios.html')
+
+def desc_bags(request):
+    return render(request, 'core/desc-bags.html')
+
+def desc_gamerstyle(request):
+    return render(request, 'core/desc-gamerstyle.html')
+
+def desc_outfits(request):
+    return render(request, 'core/desc-outfits.html')
+
+def desc_papeleria(request):
+    return render(request, 'core/desc-papeleria.html')
+
+def desc_phonecase(request):
+    return render(request, 'core/desc-phonecase.html')
+
+def inosuke(request):
+    return render(request, 'core/inosuke.html')
+
+def joker(request):
+    return render(request, 'core/joker.html')
+
+def llavero(request):
+    return render(request, 'core/llavero.html')
+
+def memo(request):
+    return render(request, 'core/memo.html')
+
+def momo(request):
+    return render(request, 'core/momo.html')
+
+def muerto(request):
+    return render(request, 'core/muerto.html')
+
+def penny(request):
+    return render(request, 'core/penny.html')
+
+def pokemochila(request):
+    return render(request, 'core/pokemochila.html')
+
+def sacapuntas(request):
+    return render(request, 'core/sacapuntas.html')
+
+def sudadera(request):
+    return render(request, 'core/sudadera-av.html')
+
+    
