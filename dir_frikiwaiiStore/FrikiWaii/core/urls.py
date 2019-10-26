c967e49cc30c6b88fd3245e3721fe9b5ae333093
JElizabeth1988 <johana.elizabeth1988@gmail.com>
2019-10-26 20:45:22 -0300

    error template T_T

--- dir_frikiwaiiStore/FrikiWaii/core/urls.py
+++ dir_frikiwaiiStore/FrikiWaii/core/urls.py
@@ -1,15 +1,12 @@
 from django.urls import path
-from .views import home, alien, audifonos, bunny, car_avengers, car_naruto,
-carro, crybaby, desc_accesorios, desc_bags, desc_gamerstyle, desc_outfits,
-desc_papeleria, desc_phonecase, inosuke, joker, llavero, memo, momo, muerto,
-penny, pokemochila, sacapuntas, sudadera
+from .views import home, alien, audifonos, bunny, car_avengers, car_naruto, carro, crybaby, desc_accesorios, desc_bags, desc_gamerstyle, desc_outfits,desc_papeleria, desc_phonecase, inosuke, joker, llavero, memo, momo, muerto,penny, pokemochila, sacapuntas, sudadera
 
 urlpatterns = [
     path('', home, name="home"),
     path('Alien/', alien, name="alien"),
     path('Audifonos/', audifonos, name="audifonos"),
     path('Mascara-bunny/', bunny, name="bunny"),
-    path('Carcasa-avengers/', car_avengers, name="car-avengers"),
+    path('Carcasa-avengers/', car_avengers, name="car_avengers"),
     path('Carcasa-naruto/', car_naruto, name="car_naruto"),
     path('Carro/', carro, name="carro"),
     path('Mascara-cry-baby/', crybaby, name="crybaby"),
@@ -28,7 +25,5 @@ urlpatterns = [
     path('mascara-pennywise/', penny, name="penny"),
     path('pokemochila/', pokemochila, name="pokemochila"),
     path('sacapuntas/', sacapuntas, name="sacapuntas"),
-    path('sudadera-avengers/', sudadera, name="sudadera"),
-    
-  
+    path('sudadera-avengers/', sudadera, name="sudadera"), 
 ]
