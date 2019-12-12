0915d4fe1fda2f86c4aa8c151b2696935e7ad832
hidalgog <hidalgog@users.noreply.github.com>
2019-12-12 01:53:05 -0300

    Revert "Revert "Contacto + agregar con form + cambio bd""
    This reverts commit 130f049dae44ada5962ddbb97a59b76161bd8afd.

--- dir_frikiwaiiStore/FrikiWaii/core/forms.py
+++ dir_frikiwaiiStore/FrikiWaii/core/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 from django.forms import ModelForm
-from .models import Producto
+from .models import Producto, Contacto
 
 class ProductoForm(ModelForm):
     class Meta:
         model = Producto
         fields = ['codigo','nombre','descripcion','precio','categoria','imagen']
+
+class ContactoForm(ModelForm):
+
+    class Meta:
+        model = Contacto 
+        fields = ['nombre','apellido','asunto','tipoContacto','mensaje']
